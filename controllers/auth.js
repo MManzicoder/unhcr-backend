5ce2961baaeece94dc29bf720dfff89c2c3bbc09
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2021-11-21 20:15:36 +0200

    loginadmin

---
+++ controllers/auth.js
@@ -0,0 +1,15 @@
+const login = async (req, res)=>{
+      try {
+        let {email, password} = req.body;
+        let user = await User.findOne({email});
+        if(!user) return res.status(400).json({err: "Invalid email or password!"});
+        const isMatch = await comparePassword(password, user);
+        if(!isMatch) return res.status(400).json({err: "Invalid email or password!"});
+        let token = createToken(user._id);
+        let { _id, username, adminType } = user;
+        email = user.email;
+        return res.json({token, user:{_id, username, email, adminType}}); 
+      } catch (error) {
+          console.log("Error ...", error.message);
+      }
+}
